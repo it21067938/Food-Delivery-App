d38c48b93f3d241b0acd2a985b0f169fbc4e54ce
Thisara Dilshan <it21067938@my.sliit.lk>
2024-12-24 00:52:58 +0530

    admin dashboard name

--- admin/src/component/navbar/Navbar.jsx
+++ admin/src/component/navbar/Navbar.jsx
@@ -6,6 +6,7 @@ function Navbar() {
   return (
     <div className='navbar'>
         <img className="logo" src={assets.logo} alt="" />
+        <h3>Admin Dashboard</h3>
         <img className="profile" src={assets.profile_image} alt="" />
     </div>
   )
